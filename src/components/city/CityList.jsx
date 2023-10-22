aeb52c33e2fbb14367920ba2abad6b302601be5a
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-22 21:39:41 +0530

    🥅 feat: error handled if cities not avail

--- src/components/city/CityList.jsx
+++ src/components/city/CityList.jsx
@@ -3,9 +3,17 @@ import React from 'react';
 import styles from './CityList.module.css';
 import Spinner from '../spinner/Spinner';
 import CityItem from './CityItem';
+import Message from '../message/Message';
 
 const CityList = ({ cities, isLoading }) => {
   if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message
+        message={'Add your first city by clicking on a city on the map'}
+      />
+    );
 
   return (
     <ul className={styles.cityList}>
