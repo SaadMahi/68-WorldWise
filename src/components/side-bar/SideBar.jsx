bfe505435bd69bd1e367518b93ba355a5b4d6c91
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-22 20:06:54 +0530

    🚚 feat: renamed component folders

--- src/components/side-bar/SideBar.jsx
+++ src/components/side-bar/SideBar.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import styles from './Sidebar.module.css';
 import Logo from '../logo/Logo';
+import { Outlet } from 'react-router-dom';
+import AppNav from '../app nav/AppNav';
 
 const SideBar = () => {
   return (
     <div className={styles.sidebar}>
       <Logo />
 
-      <p>List of cities</p>
+      <AppNav />
+      <Outlet />
+
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
