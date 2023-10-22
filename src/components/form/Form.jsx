10c24a37f7239c0f724ef2357bc4161a9454e67b
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-23 03:20:53 +0530

    ⚓ feat: use useNavigate hook to go 1 step back url

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 import styles from './Form.module.css';
 import Button from '../button/Button';
+import { useNavigate } from 'react-router-dom';
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -14,6 +15,15 @@ export function convertToEmoji(countryCode) {
 }
 
 function Form() {
+  /** useNavigate HOOK TO GO ONE STEP BACK FROM CURRENT PAGE
+   * ! here we will be applying a special condtion that when the user clicks the back button on the form
+   * he will go back in the browser history
+   * so in onClick btn in navigation function we just need to define
+   * the nummber of steps that we want to go back in the browsers history
+   * well here we will be using -1 which means we need to navigate back 1 step
+   */
+  const navigate = useNavigate();
+
   const [cityName, setCityName] = useState('');
   const [country, setCountry] = useState('');
   const [date, setDate] = useState(new Date());
@@ -51,7 +61,15 @@ function Form() {
 
       <div className={styles.buttons}>
         <Button type='primary'>Add</Button>
-        <Button type='back'>&larr; Back</Button>
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            navigate(-1);
+          }}
+          type='back'
+        >
+          &larr; Back
+        </Button>
       </div>
     </form>
   );
