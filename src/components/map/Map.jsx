7c11db80f9a63efed6368ec096dc05b1fc5a2854
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-23 02:17:23 +0530

    ⚓ feat: useSearchParams hook introduced

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import styles from './map.module.css';
+import { useSearchParams } from 'react-router-dom';
 
 const Map = () => {
-  return <div className={styles.mapContainer}>map</div>;
+  /**
+   * useSearch param is a hook given by the react router
+   * it is similar to useState hook
+   * it returns an array which has the current state, second is to set param
+   *
+   * next to store or get lat and lng which is in url we use:
+   * the get method on this searchParam object and use the name of the variable
+   */
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h1>
+        Positions: {lat}, {lng}
+      </h1>
+    </div>
+  );
 };
 
 export default Map;
