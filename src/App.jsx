f34080d166cc5eda2a3e74bd83f9b81d085ec57a
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-23 11:43:06 +0530

    🐛 fix: countries were not rendering

--- src/App.jsx
+++ src/App.jsx
@@ -54,6 +54,7 @@ function App() {
           <Route path='cities/:id' element={<City />} />
 
           <Route
+            path='countries'
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
           <Route path='form' element={<Form />} />
