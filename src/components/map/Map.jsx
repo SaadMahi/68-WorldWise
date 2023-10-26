b9d3f78a0f1278be2bb7cd19c87ff8170353f807
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-26 20:48:17 +0530

    💄 feat: changed map ui

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -46,7 +46,7 @@ const Map = () => {
         className={styles.map}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
         <Marker position={mapPosition}>
