c25f7b3c9df495a02e7041763aa383459c7da3e4
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-26 21:02:40 +0530

    🐛 fix: ui mistake made previously

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -42,12 +42,12 @@ const Map = () => {
       <MapContainer
         center={mapPosition}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         className={styles.map}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
         />
         <Marker position={mapPosition}>
           <Popup>
