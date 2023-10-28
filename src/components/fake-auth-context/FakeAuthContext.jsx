8b1a2bff698658608949303351afe57625d6d806
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-28 23:22:25 +0530

    🐛 fix: wrong code for fakeAuth

--- src/components/fake-auth-context/FakeAuthContext.jsx
+++ src/components/fake-auth-context/FakeAuthContext.jsx
@@ -2,32 +2,32 @@ import { createContext, useContext, useReducer } from 'react';
 
 const AuthContext = createContext();
 
-function AuthProvider({ children }) {
-  const initialState = {
-    user: null,
-    isAuthenticated: false,
-  };
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
 
-  function reducer(state, action) {
-    switch (action.type) {
-      case 'login':
-        return { ...state, user: action.payload, isAuthenticated: true };
+function reducer(state, action) {
+  switch (action.type) {
+    case 'login':
+      return { ...state, user: action.payload, isAuthenticated: true };
 
-      case 'logout':
-        return { ...state, user: null, isAuthenticated: false };
+    case 'logout':
+      return { ...state, user: null, isAuthenticated: false };
 
-      default:
-        throw new Error('Unknown Action');
-    }
+    default:
+      throw new Error('Unknown Action');
   }
+}
 
-  const FAKE_USER = {
-    name: 'Jack',
-    email: 'jack@example.com',
-    password: 'qwerty',
-    avatar: 'https://i.pravatar.cc/100?u=zz',
-  };
+const FAKE_USER = {
+  name: 'Jack',
+  email: 'jack@example.com',
+  password: 'qwerty',
+  avatar: 'https://i.pravatar.cc/100?u=zz',
+};
 
+function AuthProvider({ children }) {
   const [{ user, isAuthenticated }, dispatch] = useReducer(
     reducer,
     initialState
