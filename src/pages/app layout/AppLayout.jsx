ed4960d9010d90ff7ad642142cdcaa5ac986b33c
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-29 01:05:28 +0530

    ✅ feat: added login and logout functionality

--- src/pages/app layout/AppLayout.jsx
+++ src/pages/app layout/AppLayout.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import SideBar from '../../components/side-bar/SideBar';
 import styles from './AppLayout.module.css';
 import Map from '../../components/map/map';
+import User from '../../components/user/User';
 
 const AppLayout = () => {
   return (
     <div className={styles.app}>
       <SideBar />
       <Map />
+      <User />
     </div>
   );
 };
