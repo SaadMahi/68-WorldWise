fe1529fa6e0297f5e2aed50a5f1ffe07a03a2f63
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-29 01:38:32 +0530

    ✨ feat: protected route added on application

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import City from './components/city/City';
 import Form from './components/form/Form';
 import { CitiesProvider } from './contexts/CitiesContext';
 import { AuthProvider } from './components/fake-auth-context/FakeAuthContext';
+import ProtectedRoute from './pages/protected-route/ProtectedRoute';
 
 // 1) Add `AuthProvider` to `App.jsx`
 
@@ -25,7 +26,14 @@ function App() {
             <Route path='/product' element={<Product />} />
             <Route path='pricing' element={<Pricing />} />
             <Route path='login' element={<Login />} />
-            <Route path='app' element={<AppLayout />}>
+            <Route
+              path='app'
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to='cities' />} />
               <Route path='cities' element={<CityList />} />
               <Route path='cities/:id' element={<City />} />
