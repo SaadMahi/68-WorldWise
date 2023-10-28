e727e4411bd0757ddf40374faefc0d790a304cbb
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-28 23:53:35 +0530

    ✅ feat: auth provided added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,28 +11,33 @@ import CountryList from './components/country/CountryList';
 import City from './components/city/City';
 import Form from './components/form/Form';
 import { CitiesProvider } from './contexts/CitiesContext';
+import { AuthProvider } from './components/fake-auth-context/FakeAuthContext';
+
+// 1) Add `AuthProvider` to `App.jsx`
 
 function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path='/product' element={<Product />} />
-          <Route path='pricing' element={<Pricing />} />
-          <Route path='login' element={<Login />} />
-          <Route path='app' element={<AppLayout />}>
-            <Route index element={<Navigate replace to='cities' />} />
-            <Route path='cities' element={<CityList />} />
-            <Route path='cities/:id' element={<City />} />
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path='/product' element={<Product />} />
+            <Route path='pricing' element={<Pricing />} />
+            <Route path='login' element={<Login />} />
+            <Route path='app' element={<AppLayout />}>
+              <Route index element={<Navigate replace to='cities' />} />
+              <Route path='cities' element={<CityList />} />
+              <Route path='cities/:id' element={<City />} />
 
-            <Route path='countries' element={<CountryList />} />
-            <Route path='form' element={<Form />} />
-          </Route>
-          <Route path='*' element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+              <Route path='countries' element={<CountryList />} />
+              <Route path='form' element={<Form />} />
+            </Route>
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 }
 
